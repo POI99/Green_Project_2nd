f8efc22fd3b5f30bf273efb57ec4b4f9a14dbb56
HOME <ljun88@naver.com>
2024-07-26 13:52:30 +0900

    [이진현] 리뷰 작성 TDD 완 리뷰 삭제 TDD 작성

--- src/test/java/com/green/glampick/service/UserServiceTest.java
+++ src/test/java/com/green/glampick/service/UserServiceTest.java
@@ -2,6 +2,7 @@ package com.green.glampick.service;
 
 import com.green.glampick.common.CustomFileUtils;
 import com.green.glampick.dto.ResponseDto;
+import com.green.glampick.dto.request.user.DeleteReviewRequestDto;
 import com.green.glampick.dto.request.user.PostReviewRequestDto;
 import com.green.glampick.dto.response.user.PostReviewResponseDto;
 import com.green.glampick.entity.ReservationCompleteEntity;
@@ -59,11 +60,11 @@ class UserServiceTest {
     }
 
     @Test
-    @DisplayName("별점")
-    void postReview() {
+    @DisplayName("별점범위 벗어난 코드 검사")
+    void postStarOutReview() {
 
         PostReviewRequestDto dto = new PostReviewRequestDto();
-        dto.setReviewStarPoint(5);
+        dto.setReviewStarPoint(8);
 
         ReviewEntity reviewEntity = new ReviewEntity();
         reviewEntity.setReviewStarPoint(dto.getReviewStarPoint());
@@ -73,17 +74,88 @@ class UserServiceTest {
         when(reviewRepository.save(reviewEntity)).thenReturn(new ReviewEntity(dto));
 // dto.getReviewStarPoint() 가  5인지 검증
         int starPoint = dto.getReviewStarPoint();
-        boolean StarPoint = (starPoint >= 0 && starPoint <= 5);
-            assertEquals(StarPoint, dto.getReviewStarPoint(), "0~5사이의 별점 입력");
-//        assertNotEquals(StarPoint, dto.getReviewStarPoint(), "0~5사이의 별점 입력");
-//        assertTrue(StarPoint, "0~5사이의 별점 입력");
-        if (starPoint < 0 || starPoint > 5) {
-            fail("0~5사이의 별점 입력");
+//        boolean StarPoint = (starPoint >= 0 && starPoint <= 5);
+        if (starPoint >= 0 && starPoint <= 5) {
+//                       assertEquals(starPoint, dto.getReviewStarPoint(), "0~5사이의 별점 입력");
+            assertNotEquals(starPoint, dto.getReviewStarPoint(), "0~5사이 입니다.");
+
+        }else {
+            assertEquals(starPoint, dto.getReviewStarPoint(), "0~5를 벗어남");
+
         }
+    }
+    @Test
+    @DisplayName("별점범위 내 코드 검사")
+    void postStarInReview() {
+
+        PostReviewRequestDto dto = new PostReviewRequestDto();
+        dto.setReviewStarPoint(3);
+
+        ReviewEntity reviewEntity = new ReviewEntity();
+        reviewEntity.setReviewStarPoint(dto.getReviewStarPoint());
+//        dto.setReservationId(93);
+//        dto.setReviewContent("Great experience!");
+// given(); 객체가 특정 상황에서 해야하는 행위를 정의하는 메소드
+        when(reviewRepository.save(reviewEntity)).thenReturn(new ReviewEntity(dto));
+// dto.getReviewStarPoint() 가  5인지 검증
+        int starPoint = dto.getReviewStarPoint();
+//        boolean StarPoint = (starPoint >= 0 && starPoint <= 5);
+        if (starPoint >= 0 && starPoint <= 5) {
+//                       assertEquals(starPoint, dto.getReviewStarPoint(), "0~5사이의 별점 입력");
+            assertEquals(starPoint, dto.getReviewStarPoint(), "0~5를 벗어남");
+
+        }else {
+
+            assertNotEquals(starPoint, dto.getReviewStarPoint(), "0~5사이 입니다.");
+        }
+    }
+    @Test
+    @DisplayName("리뷰가 있을 경우")
+    void postExistReview() {
+
+        PostReviewRequestDto dto = new PostReviewRequestDto();
+        dto.setReviewContent("리뷰 있어요!!");
+        ReviewEntity reviewEntity = new ReviewEntity();
+        reviewEntity.setReviewStarPoint(dto.getReviewStarPoint());
+//        dto.setReservationId(93);
+
+// given(); 객체가 특정 상황에서 해야하는 행위를 정의하는 메소드
+        when(reviewRepository.save(reviewEntity)).thenReturn(new ReviewEntity(dto));
+// dto.getReviewStarPoint() 가  5인지 검증
+        String review = dto.getReviewContent();
+        boolean ExistReview = (review !=null);
+        assertTrue(ExistReview,"리뷰가 있어요");
+
+
+    }
+    @Test
+    @DisplayName("리뷰가 없는 경우")
+    void postNotExistReview() {
+
+        PostReviewRequestDto dto = new PostReviewRequestDto();
+//        dto.setReviewContent();
+        ReviewEntity reviewEntity = new ReviewEntity();
+        reviewEntity.setReviewStarPoint(dto.getReviewStarPoint());
+//        dto.setReservationId(93);
+
+// given(); 객체가 특정 상황에서 해야하는 행위를 정의하는 메소드
+        when(reviewRepository.save(reviewEntity)).thenReturn(new ReviewEntity(dto));
+// dto.getReviewStarPoint() 가  5인지 검증
+        String review = dto.getReviewContent();
+        boolean ExistReview = (review ==null);
+        assertTrue(ExistReview,"리뷰가 없어요");
+
+
     }
 
     @Test
     void deleteReview() {
+        DeleteReviewRequestDto dto = new DeleteReviewRequestDto();
+        ReviewEntity reviewEntity = new ReviewEntity();
+        dto.setReviewId(20);
+        reviewEntity.setReviewId(dto.getReviewId());
+
+        when(reviewRepository.deleteById(dto.getReviewId());
     }
 
     @Test
