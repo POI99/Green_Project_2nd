7213b4e10b58670b1c1972ec2dfb46945a4b7f5a
HOME <ljun88@naver.com>
2024-07-26 09:27:18 +0900

    [이진현] TDD작성 중

--- src/test/java/com/green/glampick/service/UserServiceTest.java
+++ src/test/java/com/green/glampick/service/UserServiceTest.java
@@ -1,6 +1,7 @@
 package com.green.glampick.service;
 
 import com.green.glampick.common.CustomFileUtils;
+import com.green.glampick.dto.ResponseDto;
 import com.green.glampick.dto.request.user.PostReviewRequestDto;
 import com.green.glampick.dto.response.user.PostReviewResponseDto;
 import com.green.glampick.entity.ReservationCompleteEntity;
@@ -32,15 +33,22 @@ import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
-@Import({UserServiceImpl.class })
+@Import({UserServiceImpl.class})
 class UserServiceTest {
-    @Autowired private UserService userservice;
-    @MockBean private ReviewRepository reviewRepository;
-    @MockBean private GlampingStarRepository glampingStarRepository;
-    @MockBean private ReservationCompleteRepository reservationCompleteRepository;
-    @MockBean private ReviewImageRepository reviewImageRepository;
-    @MockBean private CustomFileUtils customFileUtils;
-    @MockBean private AuthenticationFacade authenticationFacade;
+    @Autowired
+    private UserService userservice;
+    @MockBean
+    private ReviewRepository reviewRepository;
+    @MockBean
+    private GlampingStarRepository glampingStarRepository;
+    @MockBean
+    private ReservationCompleteRepository reservationCompleteRepository;
+    @MockBean
+    private ReviewImageRepository reviewImageRepository;
+    @MockBean
+    private CustomFileUtils customFileUtils;
+    @MockBean
+    private AuthenticationFacade authenticationFacade;
 
     @Test
     void getBook() {
@@ -55,15 +63,25 @@ class UserServiceTest {
     void postReview() {
 
         PostReviewRequestDto dto = new PostReviewRequestDto();
+        dto.setReviewStarPoint(5);
 
         ReviewEntity reviewEntity = new ReviewEntity();
-        dto.setReviewStarPoint(5);
-        dto.setReservationId(93);
-        dto.setReviewContent("Great experience!");
+        reviewEntity.setReviewStarPoint(dto.getReviewStarPoint());
+//        dto.setReservationId(93);
+//        dto.setReviewContent("Great experience!");
 // given(); 객체가 특정 상황에서 해야하는 행위를 정의하는 메소드
         when(reviewRepository.save(reviewEntity)).thenReturn(new ReviewEntity(dto));
-
+// dto.getReviewStarPoint() 가  5인지 검증
+        int starPoint = dto.getReviewStarPoint();
+        boolean StarPoint = (starPoint >= 0 && starPoint <= 5);
+            assertEquals(StarPoint, dto.getReviewStarPoint(), "0~5사이의 별점 입력");
+//        assertNotEquals(StarPoint, dto.getReviewStarPoint(), "0~5사이의 별점 입력");
+//        assertTrue(StarPoint, "0~5사이의 별점 입력");
+        if (starPoint < 0 || starPoint > 5) {
+            fail("0~5사이의 별점 입력");
+        }
     }
+
     @Test
     void deleteReview() {
     }
